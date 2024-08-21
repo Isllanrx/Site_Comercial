c4956e635fee3664d79cc56ce913e6612ca28449
User <isllan.toso@hotmail.com>
2024-08-21 15:45:24 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Funcionalidade do Carrossel (se necessário)
+const carousel = document.querySelector('.carousel');
+if (carousel) {
+  // Inicializa o carrossel do Bootstrap
+  new bootstrap.Carousel(carousel);
+}
+
